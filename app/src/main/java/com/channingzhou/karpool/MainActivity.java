0c0b10df41e995d04b9ad6467b214b0720e238b6
channinz <jerr9zhou@gmail.com>
2015-04-12 18:52:32 -0500

    1.SMS same as last time 2.sign up works perfectly but login

--- app/src/main/java/com/channingzhou/karpool/MainActivity.java
+++ app/src/main/java/com/channingzhou/karpool/MainActivity.java
@@ -2,13 +2,25 @@ package com.channingzhou.karpool;
 
 import android.app.Activity;
 import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class mainActivity extends Activity implements OnClickListener {
@@ -16,6 +28,17 @@ public class mainActivity extends Activity implements OnClickListener {
     Button btnLoginDialog;
     //sign up button
     Button btnSignupDialog;
+
+    //progress Dialog
+    private ProgressDialog pDialog;
+
+    private static String url_create_user = "http://192.168.0.21/karpool/userRegister.php";
+    private static String url_login_user = "http://192.168.0.21/karpool/userLogin.php";
+
+    JSONParser jsonParser = new JSONParser();
+
+    //JSON node names
+    private static final String TAG_SUCCESS = "success";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,28 +73,82 @@ public class mainActivity extends Activity implements OnClickListener {
             final EditText txtUsername = (EditText)login.findViewById(R.id.txtUsername);
             final EditText txtPassword = (EditText)login.findViewById(R.id.txtPassword);
 
+
+
+
+
             // Attached listener for login GUI button
             btnLogin.setOnClickListener(new OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     if(txtUsername.getText().toString().trim().length() > 0 && txtPassword.getText().toString().trim().length() > 0)
                     {
+                        //validate the login info
 
-                        // Validate Your login credential here than display message
+                        new userLogin().execute();
+
+                        /*Intent i = new Intent(mainActivity.this, driverAndRider.class);
+                        startActivity(i);
+
                         Toast.makeText(mainActivity.this,
-                                "Login Sucessfull", Toast.LENGTH_LONG).show();
+                                "Login Sucessfull", Toast.LENGTH_LONG).show();*/
+
 
                         // Redirect to dashboard / home screen.
                         login.dismiss();
-
-                        Intent i = new Intent(mainActivity.this, driverAndRider.class);
-                        startActivity(i);
                     }
                     else
                     {
                         Toast.makeText(mainActivity.this,
                                 "Please enter Username and Password", Toast.LENGTH_LONG).show();
 
+                    }
+                }
+
+                class userLogin extends AsyncTask<String, String, String>{
+                    @Override
+                    protected String doInBackground(String... args) {
+
+                        String username = txtUsername.getText().toString();
+                        String password = txtPassword.getText().toString();
+
+                        List<NameValuePair> params1 = new ArrayList<NameValuePair>();
+                        params1.add(new BasicNameValuePair("user_name", username));
+
+                        //get the same user name's info
+                        JSONObject json = jsonParser.makeHttpRequest(url_login_user, "GET", params1);
+                        
+
+                        //check the json respones
+                        Log.d("Create Response", json.toString());
+
+                        //json success tag
+                        try{
+                            int success = json.getInt(TAG_SUCCESS);
+                            if(success == 1) {
+                                //successfully received user info
+                                JSONArray userInfoObj = json.getJSONArray("user_name");
+
+                                JSONObject userInfo = userInfoObj.getJSONObject(0);
+
+                                String realPassword = userInfo.getString("password");
+
+                                if (realPassword.equals(password)){
+
+                                    // Validate login credential here than display message
+                                    Toast.makeText(mainActivity.this,
+                                            "Login Sucessfull", Toast.LENGTH_LONG).show();
+
+                                    Intent i = new Intent(mainActivity.this, driverAndRider.class);
+                                    startActivity(i);
+                                }
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+
+                        return null;
                     }
                 }
             });
@@ -108,15 +185,22 @@ public class mainActivity extends Activity implements OnClickListener {
                     if (txtUsername.getText().toString().trim().length() > 0 && txtPassword.getText().toString().trim().length() > 0) {
 
                         if (txtPassword.getText().toString().equals(txtPassword2.getText().toString())) {
-                            // Validate Your signup credential here than display message
+                            //update user table
+                            new RegisterNewUser().execute();
+
+                            Intent i = new Intent(mainActivity.this, driverAndRider.class);
+                            startActivity(i);
+
                             Toast.makeText(mainActivity.this,
                                     "Sign Up Sucessfull", Toast.LENGTH_LONG).show();
+
+
 
                             // Redirect to dashboard / home screen.
                             signup.dismiss();
 
-                            Intent i = new Intent(mainActivity.this, driverAndRider.class);
-                            startActivity(i);
+
+
                         } else {
                             Toast.makeText(mainActivity.this,
                                     "Password does not match the first one", Toast.LENGTH_LONG).show();
@@ -126,6 +210,36 @@ public class mainActivity extends Activity implements OnClickListener {
                         Toast.makeText(mainActivity.this,
                                 "Please enter Username and Password", Toast.LENGTH_LONG).show();
 
+                    }
+                }
+
+                class RegisterNewUser extends AsyncTask<String, String, String> {
+                    @Override
+                    protected String doInBackground(String... args) {
+                        String username = txtUsername.getText().toString();
+                        String password = txtPassword.getText().toString();
+
+                        List<NameValuePair> params = new ArrayList<NameValuePair>();
+                        params.add(new BasicNameValuePair("user_name", username));
+                        params.add(new BasicNameValuePair("password", password));
+
+                        JSONObject json = jsonParser.makeHttpRequest(url_create_user, "POST", params);
+
+                        Log.d("Create Response", json.toString());
+
+                        try{
+                            int success = json.getInt(TAG_SUCCESS);
+                            if(success == 1){
+
+                                /*Intent i = new Intent(mainActivity.this, driverAndRider.class);
+                                startActivity(i);*/
+
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                        return null;
                     }
                 }
             });
@@ -141,6 +255,8 @@ public class mainActivity extends Activity implements OnClickListener {
         }
 
     }
+
+
 
 ///////////////////////////////////////////////////////////////////////////////
     /*@Override
